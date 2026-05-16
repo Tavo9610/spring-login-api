359cc4bc8505a7d379ce4f2809c5af3cad31957e
Tavo9610 <gtavo96.10@gmail.com>
2026-05-16 20:03:35 -0300

    fix securityy

--- src/main/java/com/gustavo/login_api/security/SecurityConfig.java
+++ src/main/java/com/gustavo/login_api/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.gustavo.login_api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -43,15 +44,13 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
 
-                        .requestMatchers(
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**",
-                                "/swagger-ui.html"
-                        ).permitAll()
-
-                        .anyRequest()
+                        .requestMatchers(HttpMethod.POST, "/users")
                         .permitAll()
-                );
+                        .anyRequest()
+                        .authenticated()
+                )
+
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
